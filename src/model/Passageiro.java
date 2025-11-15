964bbb8daf4dbb2033a13a6e29d2c2d5ac8c2de5
murilocruz77 <160737814+murilocruz77@users.noreply.github.com>
2025-11-15 20:08:38 -0300

    Update Passageiro.java
    atualização do código passageiro, classificando e instanciando eles conforme foi feito no diagrama uml.

--- src/model/Passageiro.java
+++ src/model/Passageiro.java
@@ -2,5 +2,21 @@ package model;
 //inicio do codigo da classe passageiro
 public class Passageiro {
 
+   private String nome; 
+   private int cpf;
+   private int telefone;
+   private int senha;
+   private int formasPagamento;
+
+   public Passageiro(String nome, int cpf, int telefone, int senha, int formasPagamento) {
+      this.nome = nome;
+      this.cpf = cpf;
+      this.cpf = telefone;
+      this.senha = senha;
+      this.formasPagamento = formasPagamento;
+   }
+
+
+
 
 }
