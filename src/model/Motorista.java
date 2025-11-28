b1c60cb3252e57aed4eb8265104cb2eef75f535c
murilocruz77 <160737814+murilocruz77@users.noreply.github.com>
2025-11-28 15:23:12 -0300

    Update Motorista.java
    retirando todos os getters e sets da classe motorista

--- src/model/Motorista.java
+++ src/model/Motorista.java
@@ -14,46 +14,4 @@ public class Motorista {
         this.senha = senha;
         this.placaCarro = placaCarro;
     }
-
-    public String getNome () {
-        return nome;
-    }
-
-    public void setNome (String nome) {
-        this.nome = nome;
-    }
-
-    public int getCpf () {
-        return cpf;
-    }
-
-    public void setCpf (int cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getTelefone () {
-        return telefone;
-    }
-
-    public void setTelefone (int telefone) {
-        this.telefone = telefone;
-    }
-
-    public int getSenha () {
-        return senha;
-    }
-
-    public void setSenha (int senha) {
-        this.senha = senha;
-    }
-
-    public String getPlacaCarro () {
-        return placaCarro;
-    }
-
-    public void setPlacaCarro (String placaCarro) {
-        this.placaCarro= placaCarro;
-    }
-
-
 }
