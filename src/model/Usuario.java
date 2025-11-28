d76c9ce23c7efa16ba3086e85129a11f0f73b6c0
IsraelSoares-25 <israpvphg25@gmail.com>
2025-11-28 16:42:43 -0300

    adicionando atributos e métodos

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Usuario {
+    private String nome;
+    private String cpf;
+    private int telefone;
+    private String senha;
+
+    public Usuario(String nome, String cpf, int telefone, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
