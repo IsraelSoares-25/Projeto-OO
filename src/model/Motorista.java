c7c3f216c24a77b8bb64649fb092cd8cd99869b0
fabysoouza <fabysouza0525@gmail.com>
2025-11-29 14:40:19 -0300

    chore: implementar herança em motorista, adicionar cnh, souts e getters e setters

--- src/model/Motorista.java
+++ src/model/Motorista.java
@@ -1,16 +1,22 @@
 package model;
 
-public class Motorista {
+public class Motorista extends Usuario{
+
     private boolean disponibilidade;
     private Veiculo veiculo;
+    private CNH carteiraDeHabilitacao ;
 
-    public Motorista ( Veiculo veiculo) {
-        this.disponibilidade = true; 
+    public Motorista(String cpf, String email, String nome, String senha, String telefone, CNH carteiraDeHabilitacao, Veiculo veiculo) {
+        super(cpf, email, nome, senha, telefone);
+        this.carteiraDeHabilitacao = carteiraDeHabilitacao;
         this.veiculo = veiculo;
+        this.disponibilidade = true;
     }
-     public boolean aceitarCorrida() {
+
+    public boolean aceitarCorrida() {
         if (disponibilidade) {
             disponibilidade = false;
+            System.out.println("O motorista " + getNome() + " aceitou sua corrida!"); //so pra validar msm
             return true;
         }
         return false;
@@ -18,7 +24,31 @@ public class Motorista {
 
     public void finalizarCorrida() {
         disponibilidade = true;
+        System.out.println("Motorista disponivel novamente!");
     }
 
-   
+
+    public CNH getCarteiraDeHabilitacao() {
+        return carteiraDeHabilitacao;
+    }
+
+    public void setCarteiraDeHabilitacao(CNH carteiraDeHabilitacao) {
+        this.carteiraDeHabilitacao = carteiraDeHabilitacao;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
 }
