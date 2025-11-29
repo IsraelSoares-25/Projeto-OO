2bee7fd50cb81fb05c683763a55ad7bb55fbbcdb
fabysoouza <fabysouza0525@gmail.com>
2025-11-29 17:17:23 -0300

    chore: adicionar classe CNH e pacote de excecoes

--- src/model/CNH.java
+++ src/model/CNH.java
@@ -1,5 +1,58 @@
 package model;
 
 public class CNH {
+    private String numero;
+    private String categoria;
+    private int anoValidade;
+    private int anoEmissao;
 
+    public CNH(int anoEmissao, int anoValidade, String categoria, String numero) {
+        this.anoEmissao = anoEmissao;
+        this.anoValidade = anoValidade;
+        this.categoria = categoria;
+        this.numero = numero;
+    }
+
+    public boolean isValida(){
+        int anoAtual = 2025;
+
+        if(this.anoValidade < anoAtual){
+            System.out.println("Carteira inválida! Vence em: " +this.anoValidade);
+            return false;
+        }
+        return true;
+    }
+
+
+    public int getAnoEmissao() {
+        return anoEmissao;
+    }
+
+    public void setAnoEmissao(int anoEmissao) {
+        this.anoEmissao = anoEmissao;
+    }
+
+    public int getAnoValidade() {
+        return anoValidade;
+    }
+
+    public void setAnoValidade(int anoValidade) {
+        this.anoValidade = anoValidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
