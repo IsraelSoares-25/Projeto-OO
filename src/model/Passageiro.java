d2a87c513e8404eaba0ecfcf77bfad51a100b186
fabysoouza <fabysouza0525@gmail.com>
2025-11-29 14:16:57 -0300

    fix: corrigir ligações, atribuitos e metodos errados

--- src/model/Passageiro.java
+++ src/model/Passageiro.java
@@ -1,20 +1,36 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Passageiro extends Usuario{
 
-   private String metodosPagamento ;
+   private List<String> metodosPagamento;
+   private Corrida corridaAtual;
 
    public Passageiro(String cpf, String email, String nome, String senha, String telefone, String metodosPagamento) {
       super(cpf, email, nome, senha, telefone);
-       this.metodosPagamento = metodosPagamento;
+       this.metodosPagamento = new ArrayList<>();
+       this.metodosPagamento.add(metodosPagamento);
    }
 
-   public String getMetodosPagamento() {
+   public List<String> getMetodosPagamento() {
       return metodosPagamento;
    }
 
-   public void setMetodosPagamento(String metodosPagamento) {
+   public void setMetodosPagamento(List<String> metodosPagamento) {
       this.metodosPagamento = metodosPagamento;
+   }
+
+   public Corrida getCorridaAtual() {
+      return corridaAtual;
+   }
+
+   public void solicitarCorrida(String origem, String destino){
+
+      Corrida novaCorrida = new Corrida(this, origem, destino);
+
+      this.corridaAtual= novaCorrida;
+      System.out.println("Solicitando viagem de " + origem + " para " + destino + ". Aguarde!");
    }
 }
 
