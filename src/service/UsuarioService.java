1d6ce3eb74fcb26ce9b0cef7399d98466bc61963
IsraelSoares-25 <israpvphg25@gmail.com>
2025-11-30 21:21:27 -0300

    Arraylist de Usuario e seus métodos

--- src/service/UsuarioService.java
+++ src/service/UsuarioService.java
@@ -1,4 +1,26 @@
 package service;
 
+import java.util.ArrayList;
+import model.Usuario;
+import javax.swing.*;
+
+
 public class UsuarioService {
+    private ArrayList<Usuario> usuarios;
+    public UsuarioService() {
+        usuarios = new ArrayList<>();
+    }
+    public void adicionar(Usuario usuario) {
+        usuarios.add(usuario);
+        JOptionPane.showMessageDialog(null, "Usuario adicionado com sucesso!");
+    }
+    public void remover(Usuario usuario) {
+        usuarios.remove(usuario);
+    }
+    public void listar() {
+        for  (Usuario usuario : usuarios) {
+            JOptionPane.showMessageDialog(null, "Nome: " + usuario.getNome() +
+                    "\nSenha: " + usuario.getSenha() + "Telefone: " + usuario.getTelefone());
+        }
+    }
 }
