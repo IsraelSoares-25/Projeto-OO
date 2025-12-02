b80edbb16797f0c46ac85fd1c6fd3f6cf6f9aace
IsraelSoares-25 <israpvphg25@gmail.com>
2025-12-02 18:25:31 -0300

    lógica na classe CalculadoraDistancia

--- src/utils/CalculadoraDistancia.java
+++ src/utils/CalculadoraDistancia.java
@@ -1,4 +1,9 @@
 package utils;
 
 public class CalculadoraDistancia {
+    public static double calcular(String origem,String destino){
+        System.out.printf("Origem: %s\nDestino: %s\n", origem, destino);
+        int fator = origem.length() + destino.length();
+        return (fator* 0.5)+7.0;
+    }
 }
