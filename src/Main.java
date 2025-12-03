0b8242c0830de840a0f9ed131d21efe3e175291a
IsraelSoares-25 <israpvphg25@gmail.com>
2025-12-02 21:00:18 -0300

    arrayList<> para adicionar os tipos de Usuários

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import entidades.Passageiro;
+import entidades.Usuario;
+import service.CadastrarUsuario;
+
 public class Main {
     public static void main(String[] args) {
-
+        // adicionando usuários
+        CadastrarUsuario<Usuario> cadastro = new CadastrarUsuario<>();
+        cadastro.adicionar(new Passageiro("727323728878","João15@gmail.com","João","AEIOU","619874837","Dinheiro"));
     }
 }
