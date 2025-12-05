6265990350951ccf2d6b03dca02056079f14851a
fabysoouza <fabysouza0525@gmail.com>
2025-12-05 10:49:42 -0300

    fix: Passageiro não tem motorista, gerenciamento e validção é na classe CorridaService

--- src/entidades/Passageiro.java
+++ src/entidades/Passageiro.java
@@ -1,6 +1,5 @@
 package entidades;
 import Exceções.EstadoInvalidoDaCorridaException;
-import Exceções.NenhumMotoristaException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,33 +7,36 @@ public class Passageiro extends Usuario{
 
    private List<String> metodosPagamento;
    private Corrida corridaAtual;
-   private Motorista motorista;
 
-   public Passageiro(String cpf, String email, String nome, String senha, String telefone, String metodosPagamento) {
-      super(cpf, email, nome, senha, telefone);
+
+   public Passageiro(String nome, String cpf, String email, String telefone, String senha, String metodosPagamentoInicial) {
+       super(nome, cpf, email, telefone, senha);
        this.metodosPagamento = new ArrayList<>();
-       this.metodosPagamento.add(metodosPagamento);
+       if (metodosPagamentoInicial != null && !metodosPagamentoInicial.isEmpty()){
+           this.metodosPagamento.add(metodosPagamentoInicial);
+       }
+
    }
 
    public List<String> getMetodosPagamento() {
       return metodosPagamento;
    }
 
-   public void setMetodosPagamento(List<String> metodosPagamento) {
-      this.metodosPagamento = metodosPagamento;
+   public void adicionarMetodoPagamento(String metodo) {
+        if (metodo != null && !metodo.isEmpty()) {
+            metodosPagamento.add(metodo);
+        }
    }
 
    public Corrida getCorridaAtual() {
       return corridaAtual;
    }
 
-   public void solicitarCorrida(String origem, String destino) throws NenhumMotoristaException {
-       if(!motorista.isDisponibilidade()){
-           throw new NenhumMotoristaException("Nenhum motorista disponível");
-       }
+   public void solicitarCorrida(String origem, String destino) {
        this.corridaAtual= new Corrida(this, origem, destino);
-      System.out.println("Solicitando viagem de " + origem + " para " + destino + ". Aguarde!");
+       System.out.println("Solicitando viagem de " + origem + " para " + destino + ". Aguarde!");
    }
+
    public void cancelarCorrida()throws EstadoInvalidoDaCorridaException {
        if(this.corridaAtual != null){
            this.corridaAtual.cancelarCorrida();
