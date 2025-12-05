b1b43803719e48b7da8fd561dd65bf0fb4022e6d
fabysoouza <fabysouza0525@gmail.com>
2025-12-05 10:14:51 -0300

    chore: adicionar requisito de avaliação e verificar pendencia do usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,5 +1,8 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Usuario {
     private String nome;
     private String cpf;
@@ -7,13 +10,51 @@ public abstract class Usuario {
     private String telefone;
     private String senha;
 
-    public Usuario(String cpf, String email, String nome, String senha, String telefone) {
+    private boolean possuiPendencias;
+    private List<Integer> avaliacoes = new ArrayList<>();
+    private double mediaAvaliacoes;
+
+    public Usuario(String nome, String cpf, String email, String telefone, String senha) {
+        this.nome = nome;
         this.cpf = cpf;
         this.email = email;
-        this.nome = nome;
-        this.senha = senha;
         this.telefone = telefone;
+        this.senha = senha;
+        this.possuiPendencias = false;
+        this.mediaAvaliacoes = 0.0;
     }
+
+    public void adicionarAvaliacao(int nota){
+        if (nota < 1 || nota > 5) {
+            throw new IllegalArgumentException("A nota deve estar entre 1 e 5 estrelas! .");
+        }
+
+        avaliacoes.add(nota);
+        atualizarMedia();
+    }
+    private void atualizarMedia(){
+        double soma = 0;
+
+        if (avaliacoes.isEmpty()){
+            this.mediaAvaliacoes = 0.0;
+            return;
+        }
+
+        for (int n : avaliacoes){
+            soma += n;
+        }
+
+        this.mediaAvaliacoes = soma/avaliacoes.size();
+    }
+
+    public double getMediaAvaliacoes() {
+        return mediaAvaliacoes;
+    }
+
+    public int getNumAvaliacoes{
+        return avaliacoes.size();
+    }
+
 
     public String getCpf() {
         return cpf;
@@ -53,5 +94,17 @@ public abstract class Usuario {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public boolean isPossuiPendencias() {
+        return possuiPendencias;
+    }
+
+    public void setPossuiPendencias(boolean possuiPendencias) {
+        this.possuiPendencias = possuiPendencias;
+    }
+
+    public List<Integer> getAvaliacoes() {
+        return List.copyOf(avaliacoes);
     }
 }
